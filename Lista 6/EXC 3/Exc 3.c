793d8ac3ef625529a4586762ed1f5c7ea1c9ea9f
Victor Teles <vteles@id.uff.br>
2024-05-08 15:02:21 -0300

    Atualizando
    .

--- Lista 6/EXC 3/Exc 3.c
+++ Lista 6/EXC 3/Exc 3.c
@@ -5,18 +5,21 @@ void ret_pont (char *arq_ent, char *arq_saida, char **pontuacao, int n);
 */
 #include "stdio.h"
 #include "stdlib.h"
-void ret_pont (char *arq_ent, char *arq_saida, char **pontuacao, int n){
+void ret_pont (char *arq_ent, char *arq_saida){
     FILE* fp = fopen(arq_ent,"r");
-    if (!fp) exit(1);
+    FILE* fs = fopen(arq_saida,"w");
+    if (!fp || !fs) exit(1);
     char atual[50];
     int v1 = fscanf(fp,"%s",atual);
     while(v1==1){
+        if ((atual[0] >= 33 && atual[0] <= 47) || (atual[0] >= 58 && atual[0] <= 64)) {
+        } else fprintf(fs, "%s ", atual);
         v1 = fscanf(fp,"%s",atual);
-        if ((atual >= 33 && atual <= 47 ) || (atual >= 58 && atual <= 64 )){
-            
-        }
     }
+    fclose(fp);
+    fclose(fs);
 }
 int main(){
-
+    ret_pont("Entrada.txt", "Saida.txt");
+    return 0;
 }
